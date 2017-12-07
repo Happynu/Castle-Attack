38b3eaec08948f3789d386ace82b71dc8a4a7421
Bram de Wit <bram_de_wit@live.nl>
2017-12-07 15:47:53 +0100

    Hey, dit is iets met uhm, Blob detection. Ohja en met het maken ervan

--- Assets/blobPlacer.cs
+++ Assets/blobPlacer.cs
@@ -4,16 +4,47 @@ using UnityEngine;
 
 public class blobPlacer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public GameObject blobberPrefap;
+    public Camera cam;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        foreach (Blob i in BlobTracking.Blobs)
+        foreach (Blob blob in BlobTracking.Blobs)
         {
-
+            if (OkayCheck(blob))
+            {
+                SpawnCube(blob);
+            }
         }
 	}
+
+    private bool OkayCheck(Blob _blob)
+    {
+        return (_blob.XPosition >= 0 && 
+            _blob.XPosition <= 1 && 
+            _blob.YPosition >= 0 && 
+            _blob.YPosition <= 1 && 
+            _blob.Height > 0 && 
+            _blob.Width > 0);
+    }
+
+    private void SpawnCube(Blob _blob)
+    {
+        GameObject.Instantiate(blobberPrefap, Translate(_blob.XPosition, _blob.YPosition), Quaternion.identity);
+    }
+
+    private Vector3 Translate(float x, float y)
+    {
+        float height = 2f * cam.orthographicSize;
+        float width = height * cam.aspect;
+
+        return new Vector3(((cam.transform.position.x - (2 / width)) + (x * width)),
+            ((cam.transform.position.y - (2 / height)) + (y * height)));
+    }
 }
