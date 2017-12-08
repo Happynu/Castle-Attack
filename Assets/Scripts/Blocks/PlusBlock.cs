e729aa75c264dd6b1aa0c42a58388a823a32e2df
Jorin Tielen <jorintielen999@gmail.com>
2017-12-08 15:45:34 +0100

    Renamed PlusBlock

--- Assets/Scripts/Blocks/PlusBlock.cs
+++ Assets/Scripts/Blocks/PlusBlock.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractTest : Interactable
+public class PlusBlock : Interactable
 {
     // Use this for initialization
     void Start ()
