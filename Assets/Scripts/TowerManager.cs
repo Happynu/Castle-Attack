c438308154fa38463a30b0216ed09106cf9e78cd
Sander Boelaars <s.boelaars@student.fontys.nl>
2018-01-11 14:34:54 +0100

    Commit: Tower + Scene

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -2,99 +2,100 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TowerManager : MonoBehaviour {
+public class TowerManager : MonoBehaviour
+{
+    List<GameObject> bricks = new List<GameObject>();
+    private Transform cameraDestination;
+    private bool cameraMoving;
+    [SerializeField]
+    private float cameraStep;
 
-	List<GameObject> bricks = new List<GameObject>();
-	public Camera mainCamera;
-	private bool collapsing;
-	private int stage;
+    [SerializeField]
+    private float collapseForceMin;
 
-	// Use this for initialization
-	void Start () {
-		foreach (GameObject brick in GameObject.FindGameObjectsWithTag("Brick")) {
-			bricks.Add (brick);
-			brick.GetComponent<Rigidbody> ().isKinematic = true;
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
+    [SerializeField]
+    private float collapseForceMax;
+
+    // Use this for initialization
+    void Start()
+    {
+        cameraDestination = GameObject.Find("CameraDestination").transform;
+        foreach (GameObject brick in GameObject.FindGameObjectsWithTag("Brick"))
+        {
+            bricks.Add(brick);
+            brick.GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
             RaycastHit hit;
-			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			if (Physics.Raycast (ray, out hit)) {
-				if (hit.collider.gameObject.GetComponent<Brick> () != null) {                 
-					hit.collider.gameObject.GetComponent<Brick> ().IncreaseDamageType ();
-					foreach (Collider brickCollider in Physics.OverlapSphere(hit.transform.position, 10)) {
-						if (brickCollider.gameObject.GetComponent<Brick> () != null && hit.collider.gameObject != brickCollider.gameObject) {
-							brickCollider.gameObject.GetComponent<Brick> ().weakened = true;
-						}
-					}
-				}
-			}
-		}
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.GetComponent<Brick>() != null)
+                {
+                    hit.collider.gameObject.GetComponent<Brick>().IncreaseDamageType();
+                    foreach (Collider brickCollider in Physics.OverlapSphere(hit.transform.position, 10))
+                    {
+                        if (brickCollider.gameObject.GetComponent<Brick>() != null && hit.collider.gameObject != brickCollider.gameObject)
+                        {
+                            brickCollider.gameObject.GetComponent<Brick>().weakened = true;
+                        }
+                    }
+                }
+            }
+        }
 
-		if (Input.GetKeyDown(KeyCode.R)) {
-			foreach (GameObject brick in bricks) {
-				//if (brick.GetComponent<Brick>().weakened == true) {
-					brick.GetComponent<Rigidbody> ().isKinematic = false;
-				//}
-				StartCoroutine (DestroyBricks ());
-			}
-		}
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            cameraMoving = true;
+            foreach (GameObject brick in bricks)
+            {
+                if (brick != null)
+                {
+                    brick.GetComponent<Rigidbody>().isKinematic = false;
+                }
+            }
+            StartCoroutine(DestroyBricks());
+        }
 
-		if (Input.GetKeyDown (KeyCode.T)) {
-			NextStage ();
-		}
-	}
+        if (cameraMoving)
+        {
+            Camera.main.transform.position = Vector3.MoveTowards(Camera.main.transform.position, cameraDestination.position, cameraStep);
+            if (Camera.main.transform.position == cameraDestination.position)
+            {
+                cameraMoving = false;
+            }
+        }
+    }
 
-	public void NextStage(){
-		stage += 1;
-		if (stage > 5) {
-			//Game Over
-		}
-		MoveCameraToStage ();
-	}
+    public void Collapse()
+    {
+        Collider[] colliders = Physics.OverlapSphere(GameObject.Find("ExplosionPosition").transform.position, 20f);
+        foreach (Collider collider in colliders)
+        {
+            Rigidbody body = collider.GetComponent<Rigidbody>();
+            if (body != null)
+            {
+                body.AddExplosionForce(Random.Range(collapseForceMin, collapseForceMax), GameObject.Find("ExplosionPosition").transform.position, 50);
+            }
+        }
+    }
 
-	public void MoveCameraToStage(){
-		int cameraYLocation = 270;
-		switch (stage) {
-		case 1:
-			cameraYLocation = 270;
-			break;
-		case 2:
-			cameraYLocation = 210;
-			break;
-		case 3: 
-			cameraYLocation = 150;
-			break;
-		case 4:
-			cameraYLocation = 90;
-			break;
-		case 5: 
-			cameraYLocation = 30;
-			break;
-		}
-		mainCamera.transform.position = new Vector3 (mainCamera.transform.position.x, cameraYLocation, mainCamera.transform.position.z);
-	}
-
-	public void Collapse(){
-		Collider[] colliders = Physics.OverlapSphere (GameObject.Find ("ExplosionPosition").transform.position, 200f);
-		foreach (Collider collider in colliders) {
-			Rigidbody body = collider.GetComponent<Rigidbody> ();
-			if (body != null) {
-				body.AddRelativeForce (transform.forward * 2f, ForceMode.Force);
-			}
-		}
-	}
-
-	IEnumerator DestroyBricks(){
-		foreach (GameObject brick in bricks) {
-			if (brick != null && brick.GetComponent<Brick> ().damageType == Damage.Heavy) {
-				Destroy (brick.gameObject);
-				yield return new WaitForSecondsRealtime (.1f);
-			}
-		}
-		Collapse ();
-	}
+    IEnumerator DestroyBricks()
+    {
+        foreach (GameObject brick in bricks)
+        {
+            if (brick != null && brick.GetComponent<Brick>().damageType == Damage.Heavy)
+            {
+                Destroy(brick.gameObject);
+                yield return new WaitForSecondsRealtime(.1f);
+            }
+        }
+        Collapse();
+    }
 }
